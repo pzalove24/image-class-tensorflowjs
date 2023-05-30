135fa47fb2e6bcc84d999b9a746020e4fb727ea4
Peeratchai Kleebbua <p-ratchai@hotmail.com>
2023-05-30 14:58:20 +0700

    UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { useState, useEffect , useRef} from 'react';
 import * as mobilenet from '@tensorflow-models/mobilenet';
+import { Loader } from './components';
+
 
 function App() {
   const [isModelLoading, setisModelLoading] = useState(false);
@@ -7,6 +9,7 @@ function App() {
   const [imageURL, setImageURL] = useState(null)
   const [results, setResults] = useState([])
   const [history, setHistory] = useState([])
+  
 
   const imageRef = useRef();
   const textInputRef = useRef();
@@ -61,14 +64,22 @@ function App() {
   }, [imageURL])
   
   if (isModelLoading) {
-    return <h2>Model Loading...</h2>
+    return <h2 
+    className='flex justify-center items-center'>
+      Model Loading...
+      <p><Loader /></p>
+    </h2>
   }
 
   console.log(results)
 
   return (
-    <div className="App">
-      <h1>Image Identification</h1>
+    <div className="w-full flex 
+    justify-between items-center 
+    bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+      <h1 className='font-extrabold text-[#263caa] text-[32px]'>
+        Image Identification
+      </h1>
       <div 
         className='inputHolder'>
         <input 
